88f7359d723776fe2e2c6f7e76ca7a89e948a641
myrtokonst <myrtokonst@gmail.com>
2019-05-09 09:59:39 +0100

    nothing

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ const endingTime = document.querySelector('#ending-time')
 const activityLocation = document.querySelector('#pac-input')
 const timelineBtn = document.querySelector('#timeline-btn')
 
-// toggle form 
+// toggle form
 const toggleAddActivityForm = () => {
   if (activityForm.style.display === 'none') {
     activityForm.style.display = 'block'
@@ -48,7 +48,7 @@ const toggleTimeline = () => {
 }
 
 // Display timeline
-const counter = 0 
+const counter = 0
 timelineBtn.addEventListener('click', () => {
   const timelineList = document.createElement('ul')
   timelineList.className = "timeline"
@@ -64,7 +64,6 @@ timelineBtn.addEventListener('click', () => {
             <div class="timeline-body">
                 <p>${beginningTime.value} - ${endingTime.value}: ${activityName.value} at ${activityLocation.value}</p>
                 <p>This activity will take you ${activityDuration()} hour(s)</p>
-  
             </div>
             <div class="timeline-footer">
                 <p class="text-right">May-8-2019</p>
@@ -72,12 +71,14 @@ timelineBtn.addEventListener('click', () => {
       </div>
     </li>
   `
-  timelineDiv.append(timelineList)  
+  timelineDiv.append(timelineList)
 })
+//should we add the location and/or a little map?^
+
 
 // Display activity
 const displayUserActivity = (user) => {
-  
+
 
 }
 
@@ -94,20 +95,20 @@ const displayActivities = (activities) => {
     // add activity to summary
     const activityList = document.createElement('ul')
     activityList.className = "list-group"
-  
+
     activityList.innerHTML = `
       <li class="list-group-item">${beginningTime.value} - ${endingTime.value}: ${activityName.value} - Where? ${activityLocation.value} - Duration? ${activityDuration()} hour(s)<input type="button" class="btn orange" value="Delete" id='delete-btn' style="float: right;"></li>
-    
+
     `
     // delete activity
     const deleteBtn = activityList.querySelector('#delete-btn')
     deleteBtn.addEventListener('click', () => {
       activityList.remove()
     })
-  
+
     summaryDiv.append(activityList)
-    reset() 
-  
+    reset()
+
   })
 
 const reset = () => {
@@ -125,12 +126,12 @@ const timeStringToFloat = (time) => {
   return hours + minutes / 60;
 }
 
-// calculate duration 
+// calculate duration
 const activityDuration = () => {
   return timeStringToFloat(endingTime.value) - timeStringToFloat(beginningTime.value)
 }
 
-// init google maps 
+// init google maps
 function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 51.520416, lng: -0.087607},
@@ -198,7 +199,7 @@ function initAutocomplete() {
     });
   }
 
-  // server 
+  // server
 
   const getUserData = (user) => {
     return fetch(`${URL}${user.id}`)
@@ -210,13 +211,12 @@ function initAutocomplete() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify(user)
-  }).then(resp => resp.json())   
+  }).then(resp => resp.json())
   }
 
   const init = () => {
-    getUserData(user) 
+    getUserData(user)
 
   }
 
   init()
-
